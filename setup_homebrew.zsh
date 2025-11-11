475bcb910c3eb50f3190a09716e591a8bfdc4de2
Oso Rojo <63561423+0sorojo@users.noreply.github.com>
2025-11-11 16:10:26 -0500

    clean up

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,11 +4,4 @@ echo "\n<<<< starting homebrew set up >>>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brew install httpie
-# brew install bat
-
-# brew install --no-quarantine iterm2
-# brew install --no-quarantine google-chrome
-# brew install --no-quarantine visual-studio-code
-
 brew bundle --verbose
